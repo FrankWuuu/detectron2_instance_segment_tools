3a1e2f258cf1f24cf0767fe444af0d5253975f10
FrankWuuu <frankwu2029@gmail.com>
2024-03-17 22:10:16 +0800

    update

--- tools/pycocoDisplay.py
+++ tools/pycocoDisplay.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 import pylab
 import os
 from PIL import Image
+import cv2
 
 coco = COCO('tooth_ins/UESB_t/annotations/test.json')
 img_dir = 'tooth_ins/UESB_t/test'
-image_id = 178
+image_id = 85
 
 img = coco.imgs[image_id]
+file_name = img["file_name"]
+print(file_name)
 # loading annotations into memory...
 # Done (t=12.70s)
 # creating index...
@@ -31,6 +34,16 @@ mask = coco.annToMask(anns[0])
 for i in range(len(anns)):
     mask += coco.annToMask(anns[i])
 
+# new_a = mask/2*255
+new_a=np.where(mask==0,0,1)*255
+
+
 plt.imshow(mask)
-plt.show()
+cv2.imwrite("mask.jpg",new_a)
+
+plt.savefig(file_name+'_mask.jpg')
+# plt.axes("off")
+plt.axis('off')
+plt.savefig("demo1.jpg", bbox_inches="tight")
+# plt.show()
 
